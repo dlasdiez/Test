12fd8e09a166f60c9e784e4b65579327b7a041fe
dlasdiez <dlasdiez@gmail.com>
2015-06-22 22:48:10 +0200

    Test

---
+++ Definitions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lavanderia
+{
+    public class Definitions
+    {
+        public enum FormType
+        {
+            Terminal = 0,
+            Operator = 1,
+            Customer = 2,
+            Families = 3,
+        }
+
+
+        public const String TERMINALS = "lvweb001.out";
+        public const String TERMINAL_FAMILIES = "lvweb002.out";
+        public const String OPERATORS = "lvweb003.out";
+        public const String CLIENTS = "lvweb004.out";
+        public const String CHECK_DATA = "lvweb005.out";
+        public const String PRINT_VOUCHER = "lvweb006.out";
+    }
+}
